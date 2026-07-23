acdc0ead6086436bea49828c2b3fa20eeb3b44e2
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-07-23 23:27:39 +0530

    Add solution: Arithmetic Operators

---
+++ hackerrank/arithmetic-operators.cpp
@@ -0,0 +1,9 @@
+// Problem: Arithmetic Operators
+// Link: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
