9d7bc87598eac3c15a49203f88f36e4ec0e14c88
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-07-23 23:26:27 +0530

    Add solution: Python If

---
+++ hackerrank/python-if.cpp
@@ -0,0 +1,24 @@
+// Problem: Python If
+// Link: https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n%2 == 1:
+    print("Weird")
+else:
+    if 2 <= n <= 5:
+            print("Not Weird")
+    elif 6 <= n <= 20:
+            print("Weird")
+    elif n > 20:
+            print("Not Weird")
