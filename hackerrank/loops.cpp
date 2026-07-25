17f9853b29126178c5402a291ad355a3e307e3a3
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-07-25 21:07:56 +0530

    Add solution: Loops

---
+++ hackerrank/loops.cpp
@@ -0,0 +1,7 @@
+// Problem: Loops
+// Link: https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0,n):
+        print(i*i)
