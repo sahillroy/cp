1b88995450fea9f6f8f6da91a5d26763a3b40278
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-07-25 21:58:07 +0530

    Add solution: Pointer

---
+++ hackerrank/pointer.cpp
@@ -0,0 +1,23 @@
+// Problem: Pointer
+// Link: https://www.hackerrank.com/challenges/c-tutorial-pointer/problem?isFullScreen=true
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void update(int *a,int *b) {
+    // Complete this function    
+    long a1 = *a + *b;
+    long b1 = abs(*a - *b);
+    cout<<a1<<endl;
+    cout<<b1<<endl;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+
+    return 0;
+}
