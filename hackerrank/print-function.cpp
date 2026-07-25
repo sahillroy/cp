07a6aeeac5792b78783b4f0bb91c7cd6bd960ded
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-07-25 21:45:29 +0530

    Add solution: Print Function

---
+++ hackerrank/print-function.cpp
@@ -0,0 +1,7 @@
+// Problem: Print Function
+// Link: https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input()) 
+    for i in range(1, n + 1):
+        print(i, end="")
