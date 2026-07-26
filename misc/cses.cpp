ec779f300271c2b2fe863802fd9309516270d392
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-07-26 14:05:10 +0530

    Update solution: CSES

--- misc/cses.cpp
+++ misc/cses.cpp
@@ -1,4 +1,32 @@
 // Problem: CSES
-// Link: https://cses.fi/problemset/result/18097236/
+// Link: https://cses.fi/problemset/result/18098357/
 
-https://leetcode.com/problems/determine-the-minimum-sum-of-a-k-avoiding-array
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    vector<long long> arr;
+    if(n==1){
+        cout<<"1"<<endl;
+    }
+    else if(n<4){
+        cout<<"NO SOLUTION"<<endl;
+    }
+    else if(n>=4){
+        for(int i =2 ; i<=n; i+=2){
+            arr.push_back(i);
+        }
+        for(int i =1 ; i<=n; i+=2){
+            arr.push_back(i);
+        }
+    }
+ 
+    for(int i=0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }
+ 
+   return 0;
+}
