d98fb68edeba5a9ae29738236f81fc8be7c1da5e
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-08-16 14:23:00 +0530

    Add solution: Not Boring Movies

---
+++ leetcode/not-boring-movies.cpp
@@ -0,0 +1,7 @@
+// Problem: Not Boring Movies
+// Link: https://leetcode.com/problems/not-boring-movies/
+
+# Write your MySQL query statement below
+SELECT * FROM Cinema 
+WHERE id%2 = 1 AND description != 'boring'
+ORDER BY rating DESC;
