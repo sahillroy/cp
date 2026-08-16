faa0c568b056c0c26f4b029a97028a85ebec3fd3
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-08-16 14:38:56 +0530

    Add solution: Average Selling Price

---
+++ leetcode/average-selling-price.cpp
@@ -0,0 +1,4 @@
+// Problem: Average Selling Price
+// Link: https://leetcode.com/problems/average-selling-price/submissions/2108774609/?envType=study-plan-v2&envId=top-sql-50
+
+u.purchase_date BETWEEN p.start_date AND p.end_date
