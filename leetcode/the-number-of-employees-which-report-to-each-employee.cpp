268c8b762e55588398052e6caf57bed1acd54984
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-08-20 23:29:22 +0530

    Add solution: The Number of Employees Which Report to Each Employee

---
+++ leetcode/the-number-of-employees-which-report-to-each-employee.cpp
@@ -0,0 +1,8 @@
+// Problem: The Number of Employees Which Report to Each Employee
+// Link: https://leetcode.com/problems/the-number-of-employees-which-report-to-each-employee/submissions/2114295799/?envType=study-plan-v2&envId=top-sql-50
+
+select mgr.employee_id, mgr.name, COUNT(emp.employee_id) as reports_count, ROUND(AVG(emp.age)) as average_age
+from employees emp join employees mgr
+on emp.reports_to = mgr.employee_id
+group by employee_id
+order by employee_id
