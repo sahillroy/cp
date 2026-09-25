20697d23910c419bea3494bae526f7a0cd83cb8a
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 14:55:49 +0530

    Add solution: Average Salary Practice Problem in SQL Practice Queries

---
+++ codechef/average-salary-practice-problem-in-sql-practice-queries.cpp
@@ -0,0 +1,6 @@
+// Problem: Average Salary Practice Problem in SQL Practice Queries
+// Link: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP14
+
+-- your code goes here
+SELECT AVG(salary) AS avg_salary
+FROM Works;
