e74af42c020ac852a29313b0b7dff5fc9c6d00c0
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 16:32:15 +0530

    Add solution: Weather Observation Station 3

---
+++ hackerrank/weather-observation-station-3.cpp
@@ -0,0 +1,4 @@
+// Problem: Weather Observation Station 3
+// Link: https://www.hackerrank.com/challenges/weather-observation-station-3/problem
+
+SELECT DISTINCT CITY FROM STATION WHERE MOD(ID, 2) = 0;
