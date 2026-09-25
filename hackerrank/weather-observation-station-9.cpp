8f7d3e6da1736f1391e0832a594248223274f424
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 16:53:44 +0530

    Add solution: Weather Observation Station 9

---
+++ hackerrank/weather-observation-station-9.cpp
@@ -0,0 +1,6 @@
+// Problem: Weather Observation Station 9
+// Link: https://www.hackerrank.com/challenges/weather-observation-station-9/problem
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(SUBSTR(CITY, 1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
