2270ef508afb819757e909ca4c20cf4912844b3f
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 15:04:59 +0530

    Add solution: Distinct Companies Practice Problem in SQL Practice Queries

---
+++ codechef/distinct-companies-practice-problem-in-sql-practice-queries.cpp
@@ -0,0 +1,6 @@
+// Problem: Distinct Companies Practice Problem in SQL Practice Queries
+// Link: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP13
+
+-- your code goes here
+SELECT DISTINCT(company_name)
+FROM Works;
