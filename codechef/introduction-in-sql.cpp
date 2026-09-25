350212f4552371b0fba496f904db9cfa9c8a9eb0
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 17:08:59 +0530

    Add solution: Introduction in SQL

---
+++ codechef/introduction-in-sql.cpp
@@ -0,0 +1,7 @@
+// Problem: Introduction in SQL
+// Link: https://www.codechef.com/learn/course/sql/SQ00LS01/problems/GSQ01
+
+/*Click on Submit to check the output.
+Click on Next to proceed */
+
+SELECT * from employee;
