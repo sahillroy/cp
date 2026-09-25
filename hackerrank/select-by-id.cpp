a2e826ab68cc0dde2f207948d297ced51ed29bdd
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 16:22:15 +0530

    Add solution: Select By ID

---
+++ hackerrank/select-by-id.cpp
@@ -0,0 +1,6 @@
+// Problem: Select By ID
+// Link: https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+
+
+
+SELECT ID, NAME, COUNTRYCODE, DISTRICT, POPULATION FROM CITY WHERE ID = 1661;
