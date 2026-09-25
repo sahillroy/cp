611ac2fc5cdf596f2963043a3ac5522bce5ec5da
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 17:17:23 +0530

    Add solution: Inserting Data into a Table in SQL

---
+++ codechef/inserting-data-into-a-table-in-sql.cpp
@@ -0,0 +1,6 @@
+// Problem: Inserting Data into a Table in SQL
+// Link: https://www.codechef.com/learn/course/sql/SQ00LS01/problems/GSQ05
+
+INSERT INTO employee (employee_id, employee_name, department)
+VALUES (4, 'Marcus Garcia' , 'Product'),
+       (5, 'Samantha Park', 'Hr');
