ca4db9ddfb9d332f0645217cb9a22d2cbe9fd945
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 14:54:01 +0530

    Add solution: High Price of Products Practice Problem in SQL Practice Queries

---
+++ codechef/high-price-of-products-practice-problem-in-sql-practice-queries.cpp
@@ -0,0 +1,7 @@
+// Problem: High Price of Products Practice Problem in SQL Practice Queries
+// Link: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP12
+
+-- your code goes here
+SELECT product_name,category
+FROM products
+WHERE price>100.00;
