d47a86844a2b64d32318a33a72d620b21fe8eddc
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 16:50:42 +0530

    Add solution: Weather Observation Station 8

---
+++ hackerrank/weather-observation-station-8.cpp
@@ -0,0 +1,7 @@
+// Problem: Weather Observation Station 8
+// Link: https://www.hackerrank.com/challenges/weather-observation-station-8/problem
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(SUBSTR(CITY, 1, 1)) IN ('a','e','i','o','u')
+  AND LOWER(SUBSTR(CITY, LENGTH(CITY), 1)) IN ('a','e','i','o','u');
