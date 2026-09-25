2b7ef16fbbc64536b4408d1e56520626fd7b37c0
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 16:20:17 +0530

    Add solution: Revising the Select Query I

---
+++ hackerrank/revising-the-select-query-i.cpp
@@ -0,0 +1,5 @@
+// Problem: Revising the Select Query I
+// Link: https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+
+SELECT * FROM CITY
+WHERE  POPULATION>100000 AND CountryCode = 'USA';
