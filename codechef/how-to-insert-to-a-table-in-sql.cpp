8c419501121906addf53f0771dde0603bc935619
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 17:22:51 +0530

    Add solution: How to insert to a table in SQL

---
+++ codechef/how-to-insert-to-a-table-in-sql.cpp
@@ -0,0 +1,5 @@
+// Problem: How to insert to a table in SQL
+// Link: https://www.codechef.com/learn/course/sql/SQ00LS01/problems/GSQ07?tab=statement
+
+INSERT INTO employee (Employee_id,Employee_Name,Department)
+VALUES (6,'Brandon Kim','Operations');
