e9c68f140a05ab9e2ee4915ca9d6a6a896ddd86c
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 16:47:42 +0530

    Add solution: Weather Observation Station 7

---
+++ hackerrank/weather-observation-station-7.cpp
@@ -0,0 +1,10 @@
+// Problem: Weather Observation Station 7
+// Link: https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE '%a'
+   OR CITY LIKE '%e'
+   OR CITY LIKE '%i'
+   OR CITY LIKE '%o'
+   OR CITY LIKE '%u';
