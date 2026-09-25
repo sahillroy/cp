1f8e9a3e284174fefd67262680b8020ab3e28d41
Sahil Roy <159305077+sahillroy@users.noreply.github.com>
2026-09-25 16:43:07 +0530

    Add solution: Weather Observation Station 5

---
+++ hackerrank/weather-observation-station-5.cpp
@@ -0,0 +1,12 @@
+// Problem: Weather Observation Station 5
+// Link: https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+SELECT CITY, LENGTH(CITY)
+FROM STATION
+ORDER BY LENGTH(CITY), CITY
+LIMIT 1;
+
+SELECT CITY, LENGTH(CITY)
+FROM STATION
+ORDER BY LENGTH(CITY) DESC, CITY
+LIMIT 1;
